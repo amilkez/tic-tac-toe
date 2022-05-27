c8f61491b88a4492dbd3e0efc219676660f5d201
angel <angelhidalgomb@gmail.com>
2022-05-26 23:42:39 -0500

    Add a winning state

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const player1 = Player("Player1", "x");
 const player2 = Player("Player2", "o");
 
 const GameBoard = (() => {
-	const gameBoard = ["", "", "", "", "", "", "", "", ""];
+	let gameBoard = ["", "", "", "", "", "", "", "", ""];
 	const markFields = Array.from(document.querySelectorAll("[data-field]"));
 
 	return { gameBoard, markFields };
@@ -18,19 +18,21 @@ const GameBoard = (() => {
 
 const Game = (() => {
 	const { gameBoard, markFields } = GameBoard;
-	const X_CLASS = "x";
-	const CIRCLE_CLASS = "o";
 
 	let isGameActive = true;
 	let currentPlayer = player1;
 	let playerTurn;
 
-	const isValidAction = (tile) => {
-		if (!tile.textContent) {
-			return false;
-		}
-		return true;
-	};
+	const WINNING_COMBINATIONS = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
 
 	const updateBoard = (index) => {
 		gameBoard[index] = currentPlayer.playersMark;
@@ -40,20 +42,50 @@ const Game = (() => {
 		field.textContent = currentPlayer.playersMark;
 	};
 
-	const handleClick = (e) => {
-		console.log(currentPlayer);
-		const field = e.target;
-		const index = field.dataset.field;
-		updateBoard(index);
-		fillField(field);
-		changeTurn();
-	};
-
 	const changeTurn = () => {
 		if (currentPlayer === player1) {
 			currentPlayer = player2;
 		} else if (currentPlayer === player2) {
 			currentPlayer = player1;
+		}
+	};
+
+	const handleResultValidation = () => {
+		let roundWon = false;
+		for (let i = 0; i <= 7; i++) {
+			const winCon = WINNING_COMBINATIONS[i];
+			const a = gameBoard[winCon[0]];
+			const b = gameBoard[winCon[1]];
+			const c = gameBoard[winCon[2]];
+			if (a === "" || b === "" || c === "") {
+				continue;
+			}
+			if ((a === b) & (b === c)) {
+				roundWon = true;
+				break;
+			}
+		}
+
+		if (roundWon) {
+			console.log("playerwon");
+			isGameActive = false;
+			return;
+		}
+
+		if (!gameBoard.includes("")) {
+			console.log("tie");
+		}
+	};
+
+	const handleClick = (e) => {
+		const currentClass = playerTurn ? player2.playersMark : player1.playersMark;
+		const field = e.target;
+		const index = field.dataset.field;
+		if (isGameActive) {
+			updateBoard(index);
+			fillField(field);
+			handleResultValidation();
+			changeTurn();
 		}
 	};
 
